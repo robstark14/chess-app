00e3d2b1636b18c712cba70360c6d94a14bcfc2e
Jose Robelle <robellempajarin@gmail.com>
2022-06-09 00:51:41 +0800

    Add white rook move logic
    Able to move the rook piece but need to solve the problem to stop the onclick event and remove classlist for the opacity. Also, still trying to figure out how to include possible enemy capture.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 //to be deleted. Just to show the location of squares
-const squares = document.querySelectorAll(".box");
-Array.from(squares).forEach((item) => {
-  item.innerHTML += `<span>${item.id}</span>`;
-});
+// const squares = document.querySelectorAll(".box");
+// Array.from(squares).forEach((item) => {
+//   item.innerHTML += `<span>${item.id}</span>`;
+// });
 //
+// class piece{
+//   constructor(color,pattern,currentLocation){
+//     this.color=color,
+//     this.pattern=pattern,
+//     this.currentLocation=currentLocation
+//   }
 
+// }
 class Move {
   constructor(id) {
     this.id = id;
@@ -28,28 +35,46 @@ class Move {
           let row = parseInt(possibleMove[1]);
           //apply opacity on 1st square
           if (row === rowPossible + 1 && col === colPossible) {
-            item.style.opacity = "0.2";
+            item.classList.toggle("valid-move");
           }
           //apply opacity on 2nd square
           if (row === rowPossible + 2 && col === colPossible) {
-            item.style.opacity = "0.2";
+            item.classList.toggle("valid-move");
           }
         });
       }
+      //move pattern for the white rook
       if (e.currentTarget.className === "w-rook") {
         Array.from(squares).filter((item) => {
           const possibleMove = item.id.split("");
           let col = possibleMove[0];
           let row = parseInt(possibleMove[1]);
           //apply opacity on 1st square
-          for (let i = 1; i < 8; i++) {
-            if (
-              // item.hasChildNodes() === false &&
-              row === rowPossible + i &&
-              col === colPossible
-            ) {
-              item.style.opacity = "0.2";
-              console.log("roooookkkkk");
+          let adjacentPiece = document.getElementById(
+            colPossible + (rowPossible + 1).toString()
+          );
+          // let blackOnPossibleCapture = document.querySelectorAll(".black");
+          // let possibleCapture = document.getElementById(
+          //   colPossible + (rowPossible + 1).toString()
+          // );
+          // let possibleCapture=Array.from(blackOnPossibleCapture).firstChild.id
+          if (!adjacentPiece.hasChildNodes()) {
+            for (let i = 1; i < 8; i++) {
+              if (
+                !item.hasChildNodes() &&
+                row === rowPossible + i &&
+                col === colPossible
+              ) {
+                item.classList.toggle("valid-move");
+                item.onclick = () => {
+                  item.innerHTML = `<img class="w-rook" id="wr1" src="./images/wr.png" alt="white rook" />`;
+                  pieceID.parentNode.removeChild(pieceID);
+                };
+                if (!e.currentTarget.parentNode.hasChildNodes()) {
+                  break;
+                }
+                console.log("roooookkkkk");
+              }
             }
           }
         });
@@ -61,4 +86,4 @@ class Move {
 //Just to run the method. How do we make this new instance argument dynamic?
 const piece = new Move("wr1");
 
-piece.getPossibleMove();
+piece.getWhitePossibleMoves();
