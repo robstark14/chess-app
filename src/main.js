84f7a9b797029e54c3defb6747c0d811fe0767f9
Jose Robelle <robellempajarin@gmail.com>
2022-06-08 21:08:24 +0800

    Add some rook move pattern logic and fix white king position

--- src/main.js
+++ src/main.js
@@ -10,17 +10,17 @@ class Move {
     this.id = id;
   }
 
-  getPossibleMove() {
+  getWhitePossibleMoves() {
     const squares = document.querySelectorAll(".box");
 
     let pieceID = document.getElementById(this.id);
 
     pieceID.onclick = (e) => {
       //fow white pawn
+      const currentPieceLocation = e.currentTarget.parentNode.id;
+      let rowPossible = parseInt(currentPieceLocation[1]); //get the id number of the possible move
+      let colPossible = currentPieceLocation[0]; //get the id letter of the possible move
       if (e.currentTarget.className === "w-pawn") {
-        const currentPieceLocation = e.currentTarget.parentNode.id;
-        let rowPossible = parseInt(currentPieceLocation[1]); //get the id number of the possible move
-        let colPossible = currentPieceLocation[0]; //get the id letter of the possible move
         //filter the possible moves
         Array.from(squares).filter((item) => {
           const possibleMove = item.id.split("");
@@ -36,11 +36,29 @@ class Move {
           }
         });
       }
+      if (e.currentTarget.className === "w-rook") {
+        Array.from(squares).filter((item) => {
+          const possibleMove = item.id.split("");
+          let col = possibleMove[0];
+          let row = parseInt(possibleMove[1]);
+          //apply opacity on 1st square
+          for (let i = 1; i < 8; i++) {
+            if (
+              // item.hasChildNodes() === false &&
+              row === rowPossible + i &&
+              col === colPossible
+            ) {
+              item.style.opacity = "0.2";
+              console.log("roooookkkkk");
+            }
+          }
+        });
+      }
     };
   }
 }
 
 //Just to run the method. How do we make this new instance argument dynamic?
-const pawn = new Move("wp1");
+const piece = new Move("wr1");
 
-pawn.getPossibleMove();
+piece.getPossibleMove();
